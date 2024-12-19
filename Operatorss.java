1e9c02b6077e1974bba3a11f4838b4ad300ba5bd
yokeshnandhk2003 <147370191+yokeshnandhk2003@users.noreply.github.com>
2024-12-19 23:33:46 +0530

    Add files via upload

---
+++ Operatorss.java
@@ -0,0 +1,119 @@
+package Operators;
+
+public class Operatorss {
+		    public static void main(String[] args) {
+		        int a = 10, b = 5;
+		        
+		        // Addition
+		        int sum = a + b;
+		        System.out.println("Sum: " + sum);
+		        
+		        // Subtraction
+		        int difference = a - b;
+		        System.out.println("Difference: " + difference);
+		        
+		        // Multiplication
+		        int product = a * b;
+		        System.out.println("Product: " + product);
+		        
+		        // Division
+		        int quotient = a / b;
+		        System.out.println("Quotient: " + quotient);
+		        
+		        // Modulus
+		        int remainder = a % b;
+		        System.out.println("Remainder: " + remainder);
+		        
+		        
+		         //Relational operators       
+		        // Equal to
+		        System.out.println("a == b: " + (a == b));
+		                
+		         // Not equal to
+		         System.out.println("a != b: " + (a != b));
+		                
+		         // Greater than
+		         System.out.println("a > b: " + (a > b));
+		                
+		         // Less than
+		         System.out.println("a < b: " + (a < b));
+		                
+		         // Greater than or equal to
+		         System.out.println("a >= b: " + (a >= b));
+		                
+		         // Less than or equal to
+		         System.out.println("a <= b: " + (a <= b));
+		            
+		         //Logical operators
+		        
+		         boolean condition1 = true, condition2 = false;
+		        	        
+		         // AND operator
+		         System.out.println("condition1 && condition2: " + (condition1 && condition2));
+		        	        
+		         // OR operator
+		         System.out.println("condition1 || condition2: " + (condition1 || condition2));
+		        	        
+		         // NOT operator
+		         System.out.println("!condition1: " + (!condition1));
+		        	    
+		         //Assignment operators
+		         
+		         int a1 = 10;
+		        	  
+		         int b1 = a1;
+		         System.out.println("b: " + b1);
+		        	        
+		         // Addition assignment
+		         b1 += 5;
+		         System.out.println("b += 5: " + b1);
+		        	        
+		         // Subtraction assignment
+		         b1 -= 3;
+		         System.out.println("b -= 3: " + b1);
+		        	        
+		         // Multiplication assignment
+		         b1 *= 2;
+		         System.out.println("b *= 2: " + b1);
+		        	        
+		         // Division assignment
+		         b1 /= 4;
+		         System.out.println("b /= 4: " + b1);
+		        	        
+		         // Modulus assignment
+		         b1 %= 3;
+		         System.out.println("b %= 3: " + b1);
+		        	    
+		         //Bitwise Operators
+		         int a2 = 5;  
+		         int b2 = 3;  
+		        	        
+		         // AND
+		         System.out.println("a & b: " + (a2 & b2));  
+		        	        
+		         // OR
+		         System.out.println("a | b: " + (a2 | b2));  
+		        	        
+		         // XOR
+		         System.out.println("a ^ b: " + (a2^ b2));  
+		        	        
+		         // NOT
+		         System.out.println("~a: " + (~a2));  
+		        	        
+		         // Left Shift
+		         System.out.println("a << 1: " + (a2<< 1));  
+		            
+		         // Right Shift
+		         System.out.println("a >> 1: " + (a2 >> 1)); 
+		        
+	 }    
+
+}        
+		        
+		   
+		
+
+
+	
+
+
